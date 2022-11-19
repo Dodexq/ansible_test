f126ee75565ad3e1de1de219ef4d9d6c24263d74
Dodex <dodexpwnz@gmail.com>
2022-11-19 18:22:15 +0300

    update 19.11

--- ansible_hw/data/ansible-install.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update
-sudo apt upgrade -y
-sudo apt install ansible -y
-sudo apt install sshpass -y
-ssh-keygen -t rsa -f /home/vagrant/.ssh/host03 -C vagrant -b 2048 -P \"\"
-cp /home/vagrant/.ssh/host03.pub /vagrant/data/host03.pub
--- ansible_hw/data/host03.sh
+++ ansible_hw/data/host03.sh
@@ -1,2 +1,3 @@
+printf "\n" >> /home/vagrant/.ssh/authorized_keys
 cat /vagrant/data/host03.pub >> /home/vagrant/.ssh/authorized_keys
 rm /vagrant/data/host03.pub
